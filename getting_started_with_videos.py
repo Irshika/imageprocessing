a6afa88009acb8e98b56387299143f66a47460dc
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-09 15:20:41 +0530

    Uses of the isOpened() function

--- getting_started_with_videos.py
+++ getting_started_with_videos.py
@@ -2,8 +2,8 @@ import cv2
 
 capture = cv2.VideoCapture('clip1.mp4')      # read from file
 #capture = cv2.VideoCapture(0)        # index of the camera. by default either 0 or -1
-
-while(True):
+#print(capture.isOpened())                    # checking isOpened() working or not
+while(capture.isOpened()):             # check the video is open or not
     ret, frame = capture.read()                # ret - is there video is available(True or false will be saved)
                                                # frame - frame will be saved / capture the frame
 
