a95470ddc3f75246bb1d17cd20c5738ad2398349
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-09 15:09:58 +0530

    Capture video from a local file

--- getting_started_with_videos.py
+++ getting_started_with_videos.py
@@ -1,7 +1,7 @@
 import cv2
 
-#capture = cv2.VideoCapture('myfile.avi')      # read from file
-capture = cv2.VideoCapture(0)        # index of the camera. by default either 0 or -1
+capture = cv2.VideoCapture('clip1.mp4')      # read from file
+#capture = cv2.VideoCapture(0)        # index of the camera. by default either 0 or -1
 
 while(True):
     ret, frame = capture.read()                # ret - is there video is available(True or false will be saved)
