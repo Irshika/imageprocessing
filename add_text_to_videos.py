6b1c392f90ea7f01178dc164c34fc00190a1c936
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-09 20:00:11 +0530

    Added date & time to the screen

--- add_text_to_videos.py
+++ add_text_to_videos.py
@@ -1,19 +1,22 @@
 import cv2
+import datetime
 
 capture = cv2.VideoCapture(0)
 
-print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))            #associate number of WIDTH = 3
-print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))           #associate number of HEIGHT = 4
+# print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))            #associate number of WIDTH = 3
+# print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))           #associate number of HEIGHT = 4
 
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-#capture.set(3, 1208)           #same as above
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-#capture.set(4, 720)            #same as above
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# #capture.set(3, 1208)           #same as above
 
-print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-#print(capture.get(3))              #same as above
-print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#print(capture.get(4))              #same as above
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# #capture.set(4, 720)            #same as above
+
+# print(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+# #print(capture.get(3))              #same as above
+
+# print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# #print(capture.get(4))              #same as above
 
 while(capture.isOpened()):
     ret, frame = capture.read()
@@ -21,7 +24,8 @@ while(capture.isOpened()):
 
         font = cv2.FONT_HERSHEY_SIMPLEX
         text = 'Width: '+ str(capture.get(cv2.CAP_PROP_FRAME_WIDTH)) + ' Height: ' + str(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-        frame = cv2.putText(frame, text,(10, 50), font, 1, (0,255,255), 2, cv2.LINE_AA )
+        date_time = str(datetime.datetime.now())
+        frame = cv2.putText(frame, date_time,(10, 50), font, 1, (0,255,255), 2, cv2.LINE_AA )
         cv2.imshow('Frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
