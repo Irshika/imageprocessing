2559587795cd137c66f8cf947af13bbbda72e7a3
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-09 19:53:13 +0530

    Added Width & Height to the video frame as text

--- add_text_to_videos.py
+++ add_text_to_videos.py
@@ -18,8 +18,11 @@ print(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 while(capture.isOpened()):
     ret, frame = capture.read()
     if ret == True:
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Frame', gray)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'Width: '+ str(capture.get(cv2.CAP_PROP_FRAME_WIDTH)) + ' Height: ' + str(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+        frame = cv2.putText(frame, text,(10, 50), font, 1, (0,255,255), 2, cv2.LINE_AA )
+        cv2.imshow('Frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
