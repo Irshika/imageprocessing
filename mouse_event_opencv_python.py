2216de2bd888b17c615b1efcd59dbcc8cfb0dbb9
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-10 22:18:33 +0530

    Right mouse button click function created

--- mouse_event_opencv_python.py
+++ mouse_event_opencv_python.py
@@ -10,16 +10,33 @@ def click_event(event, x, y, flags, param):
         print(x, ', ', y)
         font = cv2.FONT_HERSHEY_SIMPLEX
         strxy = str(x) + ', ' + str(y)
-        cv2.putText(img, strxy, (x, y), font, 1, (255,255,0), 2)
+        cv2.putText(img, strxy, (x, y), font, 0.5, (255,255,0), 2)
+        cv2.imshow('Image', img)
+    if event == cv2.EVENT_RBUTTONDOWN:
+        blue = img[y, x, 0]
+            #arg 1 : y coordinate
+            #arg 2 : x coordinate
+            #arg 3 : channel
+        green = img[y, x, 1]
+        red = img[y, x, 2]
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+        cv2.putText(img, strBGR, (x, y), font, 0.5, (255,255,0), 2)
         cv2.imshow('Image', img)
         
 
-img = np.zeros((512, 512 , 3),np.uint8)
+img = cv2.imread('lena.jpg', 1)
+
+#img = np.zeros((512, 512 , 3),np.uint8)
 cv2.imshow('Image', img)
 
 cv2.setMouseCallback('Image', click_event)
 #arg 1 : window name
 #arg 2 : callback function 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+k = cv2.waitKey(0) & 0xFF         
+
+if k == 27 | k == ord('q'):                               
+    cv2.destroyAllWindows()                   
