66c518f190681479113da17d1d98f458f53988fb
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 14:58:55 +0530

    Trackbar as a switch

--- opencv_python_trackbar_example1.py
+++ opencv_python_trackbar_example1.py
@@ -12,6 +12,10 @@ cv2.createTrackbar('B', 'image', 0, 255, nothing)
 cv2.createTrackbar('G', 'image', 0, 255, nothing)
 cv2.createTrackbar('R', 'image', 0, 255, nothing)
 
+#creating a switch
+switch = '0 : OFF\n 1 : ON'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
 while(1):
     cv2.imshow('image', img)
     k = cv2.waitKey(1) & 0xFF
@@ -21,8 +25,12 @@ while(1):
     b = cv2.getTrackbarPos('B', 'image')
     g = cv2.getTrackbarPos('G', 'image')
     r = cv2.getTrackbarPos('R', 'image')
+    s = cv2.getTrackbarPos(switch, 'image')
 
-    img[:] = [b, g, r]
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b, g, r]    
     
 cv2.destroyAllWindows()
 
