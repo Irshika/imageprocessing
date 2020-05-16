e03cf28ed7c7a258eaa8d69a21a3597f74b872e6
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 15:12:03 +0530

    Convert to grayscale using trackbar

---
+++ opencv_python_trackbar_example2.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np 
+
+#callback function when runnes createTrackbar()
+def nothing(x):
+    print(img)
+
+cv2.namedWindow('image')
+
+cv2.createTrackbar('CP', 'image', 0, 400, nothing)
+
+#creating a switch
+switch = 'color/gray'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
+while(1):
+    img = cv2.imread('lena.jpg')
+    pos = cv2.getTrackbarPos('CP', 'image')
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, str(pos), (50, 150), font, 4, (0, 0, 255), 10)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    s = cv2.getTrackbarPos(switch, 'image')
+
+    if s == 0:
+        pass
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
+
+    img = cv2.imshow('image', img)
+    
+cv2.destroyAllWindows()
+
