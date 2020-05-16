3212b4a7b457541f6a4e452e437d6ea56df5bbf5
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 15:51:07 +0530

    detected blue color balls

--- object detection/opencv_python_object_detection.py
+++ object detection/opencv_python_object_detection.py
@@ -4,12 +4,25 @@ import numpy as np
 def nothing(x):
     pass
 
-cv2.namedWindow("Tracking")
+#cv2.namedWindow("Tracking")
 
 while True:
     frame = cv2.imread('smarties.png')
 
+    #convert to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_b = np.array([110, 50, 50])       #lower limit for hsv blue color
+    upper_b = np.array([130, 255, 255])     #upper limit for hsv blue color 
+
+    #treshold the image and get only the blue color
+    mask = cv2.inRange(hsv, lower_b, upper_b)
+
+    result = cv2.bitwise_and(frame, frame, mask = mask)
+
     cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("result", result)
 
     key = cv2.waitKey(1)
     if key == 27:
