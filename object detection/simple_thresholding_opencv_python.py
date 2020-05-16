a66b89995c0a050e8844b4154d19403b2432eef0
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 18:11:19 +0530

    matplotlib with opencv, modified the simple_thresholding_opencv_python.py

--- object detection/simple_thresholding_opencv_python.py
+++ object detection/simple_thresholding_opencv_python.py
@@ -2,6 +2,7 @@
 
 import cv2 
 import numpy as np 
+from matplotlib import pyplot as plt 
 
 img = cv2.imread('gradient.png', 0)
 _, th1 = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
@@ -10,12 +11,23 @@ _, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
 _, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
 _, th5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-cv2.imshow("Image", img)
-cv2.imshow("th1", th1)
-cv2.imshow("th2", th2)
-cv2.imshow("th3", th3)
-cv2.imshow("th4", th4)
-cv2.imshow("th5", th5)
+titles = ['Original Image', 'BINARY','BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, th1, th2, th3, th4, th5]
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+
+# cv2.imshow("Image", img)
+# cv2.imshow("th1", th1)
+# cv2.imshow("th2", th2)
+# cv2.imshow("th3", th3)
+# cv2.imshow("th4", th4)
+# cv2.imshow("th5", th5)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+plt.show()
