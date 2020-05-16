571376aed740721ad7c35758eda6613c58b47908
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 14:50:18 +0530

    color picker using trackbars created

--- opencv_python_trackbar_example1.py
+++ opencv_python_trackbar_example1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #callback function when runnes createTrackbar()
 def nothing(x):
-    print(x)
+    print(img)
 
 img = np.zeros((300, 512,3), np.uint8)
 cv2.namedWindow('image')
@@ -12,13 +12,20 @@ cv2.createTrackbar('B', 'image', 0, 255, nothing)
 cv2.createTrackbar('G', 'image', 0, 255, nothing)
 cv2.createTrackbar('R', 'image', 0, 255, nothing)
 
-# while(1):
-#     cv2.imshow('image', img)
-#     k = cv2.waitKey(0) & 0xFF
-#     if k == 27:
-#         break
-# cv2.destroyAllWindows()
+while(1):
+    cv2.imshow('image', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
 
-k = cv2.waitKey(0) & 0xFF
-if k == 27 |ord('q'):
-    cv2.destroyAllWindows()
+    b = cv2.getTrackbarPos('B', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    r = cv2.getTrackbarPos('R', 'image')
+
+    img[:] = [b, g, r]
+    
+cv2.destroyAllWindows()
+
+# k = cv2.waitKey(0) & 0xFF
+# if k == 27 |ord('q'):
+#     cv2.destroyAllWindows()
