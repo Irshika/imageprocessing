17aa4f1e1c6bda98055033f9e600f895bfeb8bfc
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-16 16:56:21 +0530

    simple tresholding

--- object detection/opencv_python_object_detection.py
+++ object detection/opencv_python_object_detection.py
@@ -4,6 +4,8 @@ import numpy as np
 def nothing(x):
     pass
 
+capture = cv2.VideoCapture(0)
+
 cv2.namedWindow("Tracking")
 cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)       #LH -> Lower Hue
 cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)       #LH -> Lower Saturation
@@ -13,7 +15,8 @@ cv2.createTrackbar("US", "Tracking", 255, 255, nothing)       #LH -> Upper Satur
 cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)       #LH -> Upper Value
 
 while True:
-    frame = cv2.imread('smarties.png')
+    #frame = cv2.imread('smarties.png')
+    _, frame = capture.read()
 
     #convert to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -42,4 +45,5 @@ while True:
     if key == 27:
         break
 
+capture.release()
 cv2.destroyAllWindows()
