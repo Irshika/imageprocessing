a1ea582f6f63c7eac545c54e4397798866895799
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-18 18:06:18 +0530

    Hough Line Transform

---
+++ object detection/hough_line_transform_opencv_python.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np 
+
+img = cv2.imread('sudoku.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+cv2.imshow('edges', edges)
+lines = cv2.HoughLines(edges, 1, np.pi /180, 200)
+    #arg 1: image - source image
+    #arg 2: lines - output vector of lines. Each line is represented by a 2 or 3 element vector(rho, theta) or (rho, theta, votes).
+    #               "rho" is the distance from the coordinate origin (0, 0) (top-left corner of the image).
+    #               "theta" is the line rotation angle in radians.
+    #               "votes" is the value of accumulator
+    #arg 3: rho - Distance resolution of the accumulator in pixels
+    #arg 4: theta - Angle resolution of the accumulator in radians
+    #arg 5: threshold - Accumulator threshold parameter. Only those lines are returned that get enough votes. (>threshold)
+
+for line in lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b* rho
+
+    #x1 stores the rounded off value of (rho * cos(theta) - 1000 * sin(theta))
+    x1 = int(x0 + 1000 * (-b))
+    #y1 stores the rounded off value of (rho * sin(theta) + 1000 * cos(theta))
+    y1 = int(y0 + 1000 * (a))
+    #x2 stores the rounded off value of (rho * cos(theta) + 1000 * sin(theta))
+    x2 = int(x0 - 1000 * (-b))
+    #y2 stores the rounded off value of (rho * sin(theta) - 1000 * cos(theta))
+    y2 = int(y0 - 1000 * (a))
+
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+cv2.imshow('image', img)
+k = cv2.waitKey(0)
+cv2.destroyAllWindows()
--- object detection/hough_transform_opencv_python.py
+++ object detection/hough_transform_opencv_python.py
@@ -4,40 +4,40 @@
     #for example :
         #in the cartesian coordinate system - y = mx + c
         #in the polar coordinate system - xcos(theta) + ysin(theta) = r
-#uncomment line 8 to 27 to get an insight,
-# import cv2
-# from matplotlib import pyplot as plt
+#------------------------------ !NOTES ------------------------------------
+import cv2
+from matplotlib import pyplot as plt
 
-# img1 = cv2.imread('hough_transformation1.jpg', -1)
-# img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+img1 = cv2.imread('hough_transformation1.jpg', -1)
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 
-# img2 = cv2.imread('hough_transformation2.png')
-# img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img2 = cv2.imread('hough_transformation2.png')
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 
-# img3 = cv2.imread('hough_transformation3.jpg')
-# img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+img3 = cv2.imread('hough_transformation3.jpg')
+img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
 
-# img4 = cv2.imread('polar_coordinates1.jpg')
-# img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
+img4 = cv2.imread('polar_coordinates1.jpg')
+img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
 
-# img5 = cv2.imread('polar_coordinates2.jpg')
-# img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
+img5 = cv2.imread('polar_coordinates2.jpg')
+img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
 
-# img6 = cv2.imread('polar_coordinates3.jpg')
-# img6 = cv2.cvtColor(img6, cv2.COLOR_BGR2RGB)
+img6 = cv2.imread('polar_coordinates3.jpg')
+img6 = cv2.cvtColor(img6, cv2.COLOR_BGR2RGB)
 
-# titles = ['Cartesian Coordinates', 'Polar Coordinates', 'Example 1 (Cartesian)', 'Example 2 (cartesian)', 'Example 3 (with one x,y coordinates/polar)', 'Example 4 (with multiple x,y coordinates/polar)']
-# images = [img2, img4, img2, img3, img5, img6]
+titles = ['Cartesian Coordinates', 'Polar Coordinates', 'Example 1 (Cartesian)', 'Example 2 (cartesian)', 'Example 3 (with one x,y coordinates/polar)', 'Example 4 (with multiple x,y coordinates/polar)']
+images = [img2, img4, img2, img3, img5, img6]
 
-# for i in range(6):
-#     plt.subplot(2, 3, i+1), plt.imshow(images[i])
-#     plt.title(titles[i])
-#     plt.xticks([]), plt.yticks([])
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i])
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
 
-# plt.show() 
+plt.show() 
 
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 #using hough transform we can express entire function (with x, y coordinates) in a single point (with m, c coordinates), vise-versa
 #hough transform represents a line in form of a point in mc space(hough space)
@@ -51,3 +51,4 @@
 #opencv implements two types of hough line transform methods
     #1 - The Standard Hough Transform (HoughLines method)
     #2 - The Probabilistic Hough Line (Transform HoughLinesP method)
+
