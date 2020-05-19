de07e4d9748b90d87ffbd437538eedc36ff52cba
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-05-19 16:59:40 +0530

    edges detected using canny

--- lane detection/detector.py
+++ lane detection/detector.py
@@ -2,6 +2,16 @@ import matplotlib.pylab as plt
 import cv2
 import numpy as np 
 
+#function to mask every other things other than our roi
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)          
+    channel_count = img.shape[2]        #get number of color channels
+    match_mask_color = (255,) * channel_count            #create a match color as the same as channel_count
+    cv2.fillPoly(mask, vertices, match_mask_color)            #polygon 
+    masked_image = cv2.bitwise_and(img, mask)            #return the image only where the mask pixels matches
+    return masked_image
+
+
 image = cv2.imread('road.png')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -16,18 +26,13 @@ region_of_interest_vertices = [
     (width, height)
 ]
 
-#function to mask every other things other than our roi
-def region_of_interest(img, vertices):
-    mask = np.zeros_like(img)          
-    channel_count = img.shape[2]        #get number of color channels
-    match_mask_color = (255,) * channel_count            #create a match color as the same as channel_count
-    cv2.fillPoly(mask, vertices, match_mask_color)            #polygon 
-    masked_image = cv2.bitwise_and(img, mask)            #return the image only where the mask pixels matches
-    return masked_image
-
 image_after_masking = region_of_interest(image, 
                         np.array([region_of_interest_vertices], np.int32))
 
+#find out the edges
+gray_image = cv2.cvtColor(image_after_masking, cv2.COLOR_RGB2GRAY)
+canny_image = cv2.Canny(gray_image, 100, 200)
 
-plt.imshow(image_after_masking)
+
+plt.imshow(canny_image)
 plt.show()
